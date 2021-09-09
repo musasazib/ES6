5ae33ec5d97370ed61393fa5e92315868e7a9770
musasazib <musasazib@gmail.com>
2021-09-10 00:28:12 +0600

    Explore let & const (30-3)

---
+++ let-const.js
@@ -0,0 +1,11 @@
+let balance = 1240;
+balance = 1260;
+
+const userName = 'Golam Rabbi';
+const friend = 'Sorna' + userName;
+
+let sum = 0;
+for (let i = 0; i < Number.length; i++){
+    const number = numbers[i];
+    sum = sum + number;
+}
