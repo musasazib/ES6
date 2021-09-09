145aa2cfc5a7d53e61ec2b7348ceced62fb084a4
musasazib <musasazib@gmail.com>
2021-09-09 17:27:42 +0600

    Recap Basic JS

---
+++ recap-basic.js
@@ -0,0 +1,54 @@
+// Variable----------------------------
+var deposit = 400;
+
+// Condition----------------------------
+if (deposit > 500) {
+    
+}
+else if (deposit < 200) {
+    
+}
+else if (deposit == 500) {
+    
+}
+else if (deposit != 400) {
+    
+}
+else if (deposit >= 250) {
+    
+}
+else if (deposit <= 150) {
+    
+}
+else {
+    
+}
+
+// Array-------------------------
+const numbers = [45, 456, 8765, 355, 4, 567];
+const numberCount = numbers.length;
+numbers.pop();
+numbers.push(222);
+numbers[2] = 5555;
+// Check whether 222 included in the Array
+if (numbers.indexOf(222 != -1)) {
+    
+}
+if (numbers.includes(222)) {
+    
+}
+
+// Loop--------------------------------------
+// While loop, For Loop
+for (const number of numbers) {
+    
+}
+
+// Function -----------------------------------
+function fullName(first, second) {
+    const name = first + ' ' + second;
+}
+const person = fullName('Rahim', 'Ali');
+
+// Object------------------------------------------
+const bottle = { color: 'Yellow', contain: 'Water', price: 50 };
