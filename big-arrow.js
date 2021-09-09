8ef67aefd4ffd4a2024a6b2c66681d8873a41134
musasazib <musasazib@gmail.com>
2021-09-10 02:56:37 +0600

    Arrow functions and big arrow function (30-7)

---
+++ big-arrow.js
@@ -0,0 +1,30 @@
+const add = (num1, num2) => num1 + num2;
+const sum = add(15, 17);
+console.log(sum);
+
+const multiply = (num1, num2, num3) => num1 * num2 * num3;
+const result = multiply(4, 23, 8);
+console.log(result);
+
+
+const tenTimes = (num) => num * 10;
+const output = tenTimes(5);
+console.log(output);
+
+const fiveTimes = num => num * 10;
+const value = fiveTimes(5);
+console.log(value);
+
+const getName = () => 'Brandon Sam';
+const name = getName();
+console.log(name);
+
+const doMath = (x, y)=>{
+    const sun = x + y;
+    const diff = x - y;
+    const result = sum * diff;
+    const output = result * 5;
+    return output;
+}
+const math = doMath(5, 4);
+console.log(math);
