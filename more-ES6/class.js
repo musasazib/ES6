8fdb70dc3c97a5f9b0be64afe82b3d712e1e9b61
musasazib <musasazib@gmail.com>
2021-09-11 19:45:40 +0600

    Class, constructor, method, create object from class

---
+++ more-ES6/class.js
@@ -0,0 +1,24 @@
+class Support{
+    name;
+    designation = "Support web dev";
+    address = 'BD';
+    constructor(name, address) {
+        this.name = name;
+        this.address = address;
+    }
+    startSession() {
+        console.log(this.name, 'Start a support session');
+    }
+}
+
+const amir = new Support('Amir Khan', 'Dubai');
+const salman = new Support('Salman Khan', 'BD');
+const shaown = new Support('Showon Kumar Bissash', 'Naogaon');
+const rabbi = new Support('Golam Rabbi', 'Pahar Pur');
+const hasib = new Support('Hahiful Hasib', 'Rajshahi');
+amir.startSession();
+salman.startSession();
+shaown.startSession();
+rabbi.startSession();
+hasib.startSession();
+
