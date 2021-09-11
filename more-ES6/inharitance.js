72541802b4815558143e7f5ab4013dd2a52d92e1
musasazib <musasazib@gmail.com>
2021-09-11 19:56:22 +0600

    Inheritance Incompleted

---
+++ more-ES6/inharitance.js
@@ -0,0 +1,18 @@
+// class Support{
+//     name;
+//     designation = "Support web dev";
+//     address = 'BD';
+//     constructor(name, address) {
+//         this.name = name;
+//         this.address = address;
+//     }
+//     startSession() {
+//         console.log(this.name, 'Start a support session');
+//     }
+// }
+
+// const amir = new Support('Amir Khan', 'Dubai');
+// const salman = new Support('Salmon Khan', 'BD');
+// const shaown = new Support('Shown Kura Bissau', 'Nagano');
+// const rabbi = new Support('Gloam Rabbi', 'Pahari Pur');
+// const hasib = new Support('Hahiful Hasib', 'Rajshahi');
